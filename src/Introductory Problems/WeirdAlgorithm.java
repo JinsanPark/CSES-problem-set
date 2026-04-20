0afbbe2f55205bba80f5a3631c99972d9d80f001
JinsanPark <jmfa34@gmail.com>
2026-04-20 20:03:38 +0900

    pro1 solve

---
+++ src/Introductory Problems/WeirdAlgorithm.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(n).append(" ");
+
+        while (n != 1) {
+
+            if (n % 2 == 0) {
+
+                n /= 2;
+                sb.append(n).append(" ");
+
+            } else {
+
+                n = n * 3 + 1;
+                sb.append(n).append(" ");
+
+            }
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
