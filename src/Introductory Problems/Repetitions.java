107f9695bafc0f281bc3b3635393b35192c4f531
JinsanPark <jmfa34@gmail.com>
2026-04-22 18:19:38 +0900

    pro3 solve

---
+++ src/Introductory Problems/Repetitions.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Repetitions {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine = br.readLine();
+        char[] line = inputLine.toCharArray();
+        int n = line.length;
+        int curCount = 1;
+        int biggestCount = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i < n - 1 && line[i] == line[i + 1]) {
+
+                curCount++;
+
+            } else if (i < n - 1) {
+
+                curCount = 1;
+
+            }
+
+            if (curCount > biggestCount) {
+
+                biggestCount = curCount;
+
+            }
+
+        }
+
+        System.out.println(biggestCount);
+
+
+    }
+
+}
+
+
+//소요시간 0.18s
