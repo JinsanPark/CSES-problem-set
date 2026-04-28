f2a566cd2a3826444daa3962e62d58eed9b9049b
JinsanPark <jmfa34@gmail.com>
2026-04-28 17:32:15 +0900

    Two Knights 풀이 완

---
+++ src/Introductory Problems/TwoKnights.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TwoKnights {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int x = 2;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n ; i++) {
+
+            long square = (long)i * i;
+
+            if (i == 1) {
+                sb.append(0).append("\n");
+            } else if (i == 2) {
+                sb.append( square * (square - 1) / 2 ).append("\n");
+            } else {
+                sb.append( (square * (square - 1) / 2) - ((long) x * (x + 2)) ).append("\n");
+                x += 2;
+            }
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
